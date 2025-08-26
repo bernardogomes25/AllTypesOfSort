e63bcc070feb0a0d2ff5c3eebbd0acf5dbf37665
Gomes <be.gpereira25@gmail.com>
2025-08-26 14:27:45 -0300

    Selection Sort resolvido.

--- SortsJava/src/SelectionSort.java
+++ SortsJava/src/SelectionSort.java
@@ -8,5 +8,20 @@ public class SelectionSort {
         System.out.println("\n Esta aplicação roda o seguinte vetor: " + Arrays.toString(vetor)
                 + " e o ordena através do Selection Sort!\n");
 
+        for (int i = 0; i < vetor.length - 1; i++) {
+            int ponto = i;
+
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor[j] < vetor[ponto]) {
+                    ponto = j;
+                }
+            }
+
+            int temp = vetor[i];
+            vetor[i] = vetor[ponto];
+            vetor[ponto] = temp;
+        }
+
+        System.out.println("\n Após a ordenação, ficamos com o seguinte vetor: " + Arrays.toString(vetor));
     }
 }
