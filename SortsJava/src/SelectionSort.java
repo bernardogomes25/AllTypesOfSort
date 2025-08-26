63560a91d7deb13d2acfdea03fdaa217d95d2441
Gomes <be.gpereira25@gmail.com>
2025-08-26 15:56:38 -0300

    Comentários

--- SortsJava/src/SelectionSort.java
+++ SortsJava/src/SelectionSort.java
@@ -8,18 +8,18 @@ public class SelectionSort {
         System.out.println("\n Esta aplicação roda o seguinte vetor: " + Arrays.toString(vetor)
                 + " e o ordena através do Selection Sort!\n");
 
-        for (int i = 0; i < vetor.length - 1; i++) {
-            int ponto = i;
+        for (int i = 0; i < vetor.length - 1; i++) { //Percorre até o penúltimo índice
+            int ponto = i; //Ponto de referência
 
-            for (int j = i + 1; j < vetor.length; j++) {
-                if (vetor[j] < vetor[ponto]) {
-                    ponto = j;
+            for (int j = i + 1; j < vetor.length; j++) { //Faz um ponto de referência auxiliar percorrer o vetor ao mesmo tempo
+                if (vetor[j] < vetor[ponto]) { //Verifica se o próximo elemento é menor do que o ponto de referência atual
+                    ponto = j; //Substitui o ponto de referência pelo menor elemento a partir dele
                 }
             }
 
-            int temp = vetor[i];
-            vetor[i] = vetor[ponto];
-            vetor[ponto] = temp;
+            int temp = vetor[i]; 
+            vetor[i] = vetor[ponto]; //Faz a substituição do ponto de referência atual pelo elemento salvo
+            vetor[ponto] = temp; 
         }
 
         System.out.println("\n Após a ordenação, ficamos com o seguinte vetor: " + Arrays.toString(vetor));
