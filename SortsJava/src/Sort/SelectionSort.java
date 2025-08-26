976c3f3930beefd1f240b5d43407c08ff0c1aacd
Gomes <be.gpereira25@gmail.com>
2025-08-26 14:09:50 -0300

    Preparação para SelectionSort

---
+++ SortsJava/src/Sort/SelectionSort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] vetor = { 0, 33, 45, 83, 12, 5, 90, 11, 22 };
+
+        System.out.println("\n Esta aplicação roda o seguinte vetor: " + Arrays.toString(vetor)
+                + " e o ordena através do Selection Sort!\n");
+
+    }
+}
